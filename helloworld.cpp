e4fdea3154323efe8ce7d79050b9afe6c908e6e0
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2024-08-27 13:33:52 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//Name - Akshit Bansal
+//PRN - 23070123013
+//Aim - Printing out Hello World
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}
+/* Output:
+Hello World
+*/
